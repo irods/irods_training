c2357dfef818c3a8030b624bd82d84c73320fdd5
Ben Keller <irods.ben.keller@gmail.com>
2016-06-02 15:27:26 -0400

    package api plugin example

---
+++ advanced/irods_api_plugin/api_plugin.cmake
@@ -0,0 +1,78 @@
+set(
+  IRODS_API_PLUGIN_SOURCES_api_example_server
+  ${CMAKE_SOURCE_DIR}/src/libapi-example.cpp
+  )
+
+set(
+  IRODS_API_PLUGIN_SOURCES_api_example_client
+  ${CMAKE_SOURCE_DIR}/src/libapi-example.cpp
+  )
+
+set(
+  IRODS_API_PLUGIN_COMPILE_DEFINITIONS_api_example_server
+  RODS_SERVER
+  ENABLE_RE
+  )
+
+set(
+  IRODS_API_PLUGIN_COMPILE_DEFINITIONS_api_example_client
+  )
+
+set(
+  IRODS_API_PLUGIN_LINK_LIBRARIES_api_example_server
+  irods_server
+  irods_clerver
+  )
+
+set(
+  IRODS_API_PLUGIN_LINK_LIBRARIES_api_example_client
+  irods_client_api_table
+  irods_client_core
+  irods_client_plugins
+  )
+
+set(
+  IRODS_API_PLUGINS
+  api_example_server
+  api_example_client
+  )
+
+foreach(PLUGIN ${IRODS_API_PLUGINS})
+  add_library(
+    ${PLUGIN}
+    MODULE
+    ${IRODS_API_PLUGIN_SOURCES_${PLUGIN}}
+    )
+
+  target_include_directories(
+    ${PLUGIN}
+    PRIVATE
+    ${IRODS_INCLUDE_DIRS}
+    ${IRODS_EXTERNALS_FULLPATH_BOOST}/include
+    ${IRODS_EXTERNALS_FULLPATH_JANSSON}/include
+    ${IRODS_EXTERNALS_FULLPATH_ARCHIVE}/include
+    )
+
+  target_link_libraries(
+    ${PLUGIN}
+    PRIVATE
+    ${IRODS_API_PLUGIN_LINK_LIBRARIES_${PLUGIN}}
+    irods_plugin_dependencies
+    irods_common
+    ${IRODS_EXTERNALS_FULLPATH_BOOST}/lib/libboost_filesystem.so
+    ${IRODS_EXTERNALS_FULLPATH_BOOST}/lib/libboost_system.so
+    ${IRODS_EXTERNALS_FULLPATH_ARCHIVE}/lib/libarchive.so
+    ${OPENSSL_CRYPTO_LIBRARY}
+    )
+
+  target_compile_definitions(${PLUGIN} PRIVATE ${IRODS_API_PLUGIN_COMPILE_DEFINITIONS_${PLUGIN}} ${IRODS_COMPILE_DEFINITIONS} BOOST_SYSTEM_NO_DEPRECATED)
+  target_compile_options(${PLUGIN} PRIVATE -Wno-write-strings)
+  set_property(TARGET ${PLUGIN} PROPERTY CXX_STANDARD ${IRODS_CXX_STANDARD})
+
+  install(
+    TARGETS
+    ${PLUGIN}
+    LIBRARY
+    DESTINATION var/lib/irods/plugins/api
+    )
+endforeach()
---
+++ advanced/irods_api_plugin/icommand.cmake
@@ -0,0 +1,40 @@
+add_executable(
+  iapi_example
+  ${CMAKE_SOURCE_DIR}/src/iapi_example.cpp
+  )
+target_link_libraries(
+  iapi_example
+  PRIVATE
+  irods_client_core
+  irods_client_plugins
+  irods_client_api_table
+  irods_plugin_dependencies
+  irods_common
+  ${IRODS_EXTERNALS_FULLPATH_AVRO}/lib/libavrocpp.so
+  ${IRODS_EXTERNALS_FULLPATH_BOOST}/lib/libboost_filesystem.so
+  ${IRODS_EXTERNALS_FULLPATH_BOOST}/lib/libboost_program_options.so
+  ${IRODS_EXTERNALS_FULLPATH_BOOST}/lib/libboost_system.so
+  ${IRODS_EXTERNALS_FULLPATH_JANSSON}/lib/libjansson.so
+  ${IRODS_EXTERNALS_FULLPATH_ZMQ}/lib/libzmq.so
+  )
+target_include_directories(
+  iapi_example
+  PRIVATE
+  ${IRODS_INCLUDE_DIRS}
+  ${IRODS_EXTERNALS_FULLPATH_AVRO}/include
+  ${IRODS_EXTERNALS_FULLPATH_BOOST}/include
+  ${IRODS_EXTERNALS_FULLPATH_JANSSON}/include
+  ${IRODS_EXTERNALS_FULLPATH_ARCHIVE}/include
+  ${IRODS_EXTERNALS_FULLPATH_CPPZMQ}/include
+  ${IRODS_EXTERNALS_FULLPATH_ZMQ}/include
+  )
+target_compile_definitions(iapi_example PRIVATE RODS_SERVER ${IRODS_COMPILE_DEFINITIONS} BOOST_SYSTEM_NO_DEPRECATED)
+target_compile_options(iapi_example PRIVATE -Wno-write-strings)
+set_property(TARGET iapi_example PROPERTY CXX_STANDARD ${IRODS_CXX_STANDARD})
+
+install(
+  TARGETS
+  iapi_example
+  RUNTIME
+  DESTINATION usr/bin
+  )
