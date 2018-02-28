4e100ebf4c1571e945278c084deb9384a2750c98
Zoey Greer <tempozrene@gmail.com>
2018-02-28 13:01:46 -0500

    [#1] Change 'EXIF' import to 'exifread'

--- beginner/core.py
+++ beginner/core.py
@@ -2,14 +2,14 @@
 import os
 import session_vars
 import sys
-import EXIF
+import exifread
 def acPostProcForPut(rule_args, callback, rei):
     sv = session_vars.get_map(rei)
     phypath = sv['data_object']['file_path']
     objpath = sv['data_object']['object_path']
     exiflist = []
     with open(phypath, 'rb') as f:
-        tags = EXIF.process_file(f, details=False)
+        tags = exifread.process_file(f, details=False)
         for (k, v) in tags.iteritems():
             if k not in ('JPEGThumbnail', 'TIFFThumbnail', 'Filename', 'EXIF MakerNote'):
                 exifpair = '{0}={1}'.format(k, v)
