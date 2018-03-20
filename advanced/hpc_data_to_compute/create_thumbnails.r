83e33c03efff6459d359f6e16034e75557a5794f
Terrell Russell <terrellrussell@gmail.com>
2018-03-20 14:19:05 -0400

    change since remote() cannot send INPUTs

--- advanced/hpc_data_to_compute/create_thumbnails.r
+++ advanced/hpc_data_to_compute/create_thumbnails.r
@@ -1,11 +1,12 @@
 create_thumbnails {
-
+    *src_obj_path="/tempZone/home/rods/stickers.jpg"
+    *dst_phy_dir="/tmp/irods/thumbnails"
     *err = errormsg(create_thumbnail_collection(*src_obj_path, *dst_phy_dir), *msg)
     if(0 != *err) {
         writeLine( "stdout", "FAIL: [*err] [*msg]")
     }
 }
 
-INPUT *src_obj_path="/tempZone/home/rods/stickers.jpg",*dst_phy_dir="/tmp/irods/thumbnails"
+INPUT null
 OUTPUT ruleExecOut
 
