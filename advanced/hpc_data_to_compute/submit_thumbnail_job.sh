e764569068b383bef0af13f2d41d0bb345825a24
d-w-moore <dmoore@renci.org>
2018-04-17 05:26:47 -0400

    queries in submit/epilog now work

--- advanced/hpc_data_to_compute/submit_thumbnail_job.sh
+++ advanced/hpc_data_to_compute/submit_thumbnail_job.sh
@@ -6,6 +6,18 @@
 # $4 - source physical path
 # $5 - destination physical path
 
+# == Carry in context from slurm_prolog :
+
+# MUTX=$(hostname)_slurmjob_${SLURM_JOB_ID}.txt
+# UUID=$(iquest '%s' "select META_DATA_ATTR_VALUE where \
+# DATA_NAME = '$MUTX'  and \
+# META_DATA_ATTR_NAME = 'uuid' and \
+# COLL_NAME not like '%/trash/%'")
+# [[ $UUID = CAT_NO_ROWS_FOUND* ]] && UUID="UUID_none"
+# [ -f "/tmp/$UUID" ] && { : ; } 
+# ( /tmp/$UUID could stash private, job-related 
+#   details /  metadata on slurm controller )
+
 SBATCH_OPTIONS="-o /tmp/slurm-%j.out"
 
 SCRIPT="$1" # assume full path to executable
