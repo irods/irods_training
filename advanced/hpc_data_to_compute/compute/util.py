c3ae6e891363d8448b614b6daad7ac509a414904
d-w-moore <dmoore@renci.org>
2018-06-02 19:38:54 -0400

    enable rules and PRC to do role-based search for storage resc

--- advanced/hpc_data_to_compute/compute/util.py
+++ advanced/hpc_data_to_compute/compute/util.py
@@ -7,45 +7,58 @@ from irods.session import iRODSSession
 from irods.models import Resource , ResourceMeta
 from exceptions import SystemExit
 from . import common as _common
+import session_vars
 
-def compute_host_from_rescName (rescName ):
+def compute_host_from_rescName (rescName):
   pass
 
 chksum_opt = { k:'1' for k in ( REG_CHKSUM_KW,VERIFY_CHKSUM_KW,CHKSUM_KW ) }
 
-def put_dest_from_fileName (fileName, sess = None, config = None):
-  abs_module_path = abspath(dirname(__file__))
-  configFile =  join( abs_module_path , "put_data_to_compute.json" )
+def pyParseRoleSpec (rule_args,callback,rei):  #-- called from iRODS rule language
+    compute_resc_spec = rule_args[0]
+    rule_args[1:3]= map( lambda x:x.strip() , 
+                         (compute_resc_spec.split('=')+['']) [:2] )
+    if callback:
+      callback.writeLine ( 'serverLog', 'in pyParseRoleSpec() with map {!r}'.format(
+                           session_vars.get_map(rei)
+                         ))
 
-  if config is None:
-    config = { "dest_resource":{}, "checksums": False }
+def compute_rescName__as_rule ( compute_resc_spec, session = None, **kw ):
 
-    if os.path.isfile(configFile):
-      with open(configFile) as f:
-        config = json.load( f )
-  #-if
+ #log_method = kw.get ( 'log_method', lambda x: None ):
+  sess = kw.get ('session')
+  callback = kw.get ('callback')
+  if callback is None:
+    # not called as rule, so get session if needed
+    if session is None:  session = _common.session_object() 
+  else:
+    sess = None
 
-  ext = fileName.split(".")[-1:]
-  spec = ''
-  if ext and ext[0]:
-    spec = config["dest_resource"].get( ext[0], '' )
-
-  role_key = role_value = ''
-
+  role_key = role_value = ""
   resc = "" 
 
-  if spec:
-    if spec.find("=") > 0:
-      (role_key, role_value) = map(lambda x:x.strip() , (spec.split('=')+['']) [:2] )
-    else:
-      resc = spec
-
-  if sess and role_key:
-    q = sess.query( Resource.name , ResourceMeta.name , ResourceMeta.value )
-    q.filter (ResourceMeta.name == role_key and \
-                 ResourceMeta.value == role_value )
-    try: resc = q.one() [Resource.name]
-    except: pass
-
+  if compute_resc_spec . find("=") > 0:
+    (role_key, role_value) = map( lambda x:x.strip() , 
+                                  (compute_resc_spec.split('=')+['']) [:2] )
+  else:
+    resc = compute_resc_spec \
+           if compute_resc_spec \
+           else "demoResc"		#__ the default for now
+					#    (should be configurable, however)
+  if (role_key and role_value):
+    if sess:
+      q = sess.query( Resource.name , ResourceMeta.name , ResourceMeta.value )
+      q.filter (ResourceMeta.name == role_key and \
+                ResourceMeta.value == role_value )
+      try: resc = q.one() [Resource.name]
+      except:
+      # log_method ("query didn't work; defaulting to resc: '{}'".format(resc))
+        pass
   return str( resc )
 
+#--------------------------------------------------------------
+def compute_rescName__as_rule (args,callback,rei):
+  kw = {'callback':callback, 'rei':rei}
+  rescName = compute_rescName(*args[:1], **kw)
+  # -- return value : resource's literal name 
+  if len(args) > 1: args[1] =  rescName
