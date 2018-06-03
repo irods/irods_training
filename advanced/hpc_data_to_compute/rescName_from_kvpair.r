b03a06b1a2de03cc9cfb52ac16e6ae80b3116a61
d-w-moore <dmoore@renci.org>
2018-06-03 04:31:35 -0400

    refined some command line utilities

--- advanced/hpc_data_to_compute/rescName_from_kvpair.r
+++ advanced/hpc_data_to_compute/rescName_from_kvpair.r
@@ -1,16 +1,23 @@
 rrule {
   *out3 = ""
   *out2 = ""
-  #*in1 = "COMPUTE_RESOURCE_ROLE=IMAGE_PROCESSING"
-  *in1 = "COMPUTE_RESOURCE_ROLE=LONG_TERM_STORAGE"
+  # -- try:
+  #   irule -F rescName_from_kvpair.r "*in1='COMPUTE_RESOURCE_ROLE=LONG_TERM_STORAGE'"
+  # -- or to just get 'Default value':
+  #   irule -F rescName_from_kvpair.r <<<"" | tail -1 |sed 's/^.*=//'
+  #
+  # or as a general scheme for use in sh/Bash scripts:
+  #
+  # find_compute_resc() { irule -F rescName_from_kvpair.r <<<"\"COMPUTE_RESOURCE_ROLE=$1\"" | tail -1 |sed 's/^.*=//'; }
+  # # ...
+  # resc_name=$(find_compute_resc IMAGE_PROCESSING)
   pyParseRoleSpec( *in1, *out2, *out3 )
-  writeLine("stdout", "[ *out2 ] : [ *out3 ]" )
+  *r = "demoResc"
   if ((*out2 != "") && (*out3 != "")) {
-     *r = ""
      get_resource_name_by_role ( *r , *out2, *out3 )
-     writeLine("stdout", "[[ *r ]]" )
   }
+  writeLine("stdout", "*r" )
 }
 
-INPUT null
+INPUT  *in1=$"COMPUTE_RESOURCE_ROLE=IMAGE_PROCESSING"
 OUTPUT ruleExecOut
