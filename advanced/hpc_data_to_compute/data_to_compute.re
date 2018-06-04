fe4fad83947ddd8c41d4ac57f2e1de5ebcf2503b
d-w-moore <dmoore@renci.org>
2018-06-04 03:38:06 +0000

    reorganized rule code, mostly moved get_* funcs to /etc/irods/data_to_compute

--- advanced/hpc_data_to_compute/data_to_compute.re
+++ advanced/hpc_data_to_compute/data_to_compute.re
@@ -13,25 +13,25 @@ get_resource_name_by_role(*resc_name, *attr, *value)
      } # foreach
 }
 
-#replacing old (below:) -- with new vsn(above:)
-#get_resource_name_by_role(*resc_name, *attr, *value) {
-#     *resc_name = "demoResc"    # -- *resc_name = "NULL" # old D-to-C example
-#     if (*value == "") { # assume *attr is of form "KEY=VALUE"
-#       *out1 = ""
-#       pyParseRoleSpec ( *attr, *out1, *out2 ) # see ~irods/compute/util.py
-#     }
-#     foreach(*row in SELECT DATA_RESC_NAME WHERE META_RESC_ATTR_NAME = '*attr' AND 
-#                     META_RESC_ATTR_VALUE = '*value') {
-#         *resc_name = *row.DATA_RESC_NAME
-#     } # foreach
-# }
-
-rmtExec_slurm ( *host , *arg ) 
+get_host_and_resource_name_by_role (*host, *resc_name, *attr, *value) 
 {
-  remote("*host", "") {
-    writeLine("serverLog", "rmtExec - host [*host] remote cmd [*arg] ")
-    #msiExecCmd("hello","*arg","null","null","null",*OUT)
-  }
+    *resc_name = "NULL"    # -- *resc_name = "NULL" # old D-to-C example
+    #*value = ""
+
+    *out1=*attr
+    *out2=*value
+    if (*value == "") { # assume *attr is of form "KEY=VALUE"
+      pyParseRoleSpec ( *attr, *out1, *out2 ) # see ~irods/compute/util.py
+      writeLine ("stdout", " [ *out1 ] -> [ *out2 ] ")
+    }
+
+    foreach(*row in SELECT DATA_RESC_NAME WHERE META_RESC_ATTR_NAME = '*out1' AND 
+                    META_RESC_ATTR_VALUE = '*out2') {
+        *resc_name = *row.DATA_RESC_NAME
+    } # foreach
+
+    foreach (*h in SELECT RESC_LOC WHERE DATA_RESC_NAME = '*resc_name' )
+    {
+      *host = *h.RESC_LOC;
+    }
 }
-
-
--- advanced/hpc_data_to_compute/data_to_compute_X.r
+++ advanced/hpc_data_to_compute/data_to_compute_X.r
@@ -15,48 +15,15 @@ testRule
   writeLine ("stdout", "resc->[*resc_name] ")
 }
 
-###############################################################################
-# OLDER VERSION
-# get_resource_name_by_role(*resc_name, *attr, *value) {
-# 
-#     *resc_name =  "NULL"   # -- *resc_name = "demoResc" # old D-to-C example
-#     #*value = ""
-#     *out1 = *attr
-#     *out2 = *value
-#     if (*value == "") { # assume *attr is of form "KEY=VALUE"
-#       pyParseRoleSpec ( *attr, *out1, *out2 ) # see ~irods/compute/util.py
-#     }
-# 
-#     foreach(*row in SELECT DATA_RESC_NAME WHERE META_RESC_ATTR_NAME = '*out1' AND 
-#                     META_RESC_ATTR_VALUE = '*out2') {
-#         *resc_name = *row.DATA_RESC_NAME
-#     } # foreach
-# }
-# 
-###############################################################################
-# NEWER VERSION
-get_host_and_resource_name_by_role (*host, *resc_name, *attr, *value) {
-
-    *resc_name = "NULL"    # -- *resc_name = "NULL" # old D-to-C example
-    #*value = ""
-
-    *out1=*attr
-    *out2=*value
-    if (*value == "") { # assume *attr is of form "KEY=VALUE"
-      pyParseRoleSpec ( *attr, *out1, *out2 ) # see ~irods/compute/util.py
-      writeLine ("stdout", " [ *out1 ] -> [ *out2 ] ")
-    }
-
-    foreach(*row in SELECT DATA_RESC_NAME WHERE META_RESC_ATTR_NAME = '*out1' AND 
-                    META_RESC_ATTR_VALUE = '*out2') {
-        *resc_name = *row.DATA_RESC_NAME
-    } # foreach
-
-    foreach (*h in SELECT RESC_LOC WHERE DATA_RESC_NAME = '*resc_name' )
-    {
-      *host = *h.RESC_LOC;
-    }
+rmtExec_slurm ( *host , *arg ) 
+{
+  remote("*host", "") {
+    writeLine("serverLog", "rmtExec - host [*host] remote cmd [*arg] ")
+    #msiExecCmd("hello","*arg","null","null","null",*OUT)
+  }
 }
+
+
 
 INPUT null
 OUTPUT ruleExecOut
