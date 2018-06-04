f73adfcd4b02828ba16a5b7baf9257750a6c3e46
d-w-moore <dmoore@renci.org>
2018-06-04 01:02:15 +0000

    tightening up K=V and 'K','V' stuff, get_resky_from_roley stuff

--- advanced/hpc_data_to_compute/data_to_compute.re
+++ advanced/hpc_data_to_compute/data_to_compute.re
@@ -1,3 +1,30 @@
+get_resource_name_by_role(*resc_name, *attr, *value) 
+{
+     *resc_name =  "NULL"   # -- *resc_name = "demoResc" # old D-to-C example
+     *out1 = *attr
+     *out2 = *value
+     if (*value == "") { # assume *attr is of form "KEY=VALUE"
+       pyParseRoleSpec ( *attr, *out1, *out2 ) # see ~irods/compute/util.py
+     }
+ 
+     foreach(*row in SELECT DATA_RESC_NAME WHERE META_RESC_ATTR_NAME = '*out1' AND 
+                     META_RESC_ATTR_VALUE = '*out2') {
+         *resc_name = *row.DATA_RESC_NAME
+     } # foreach
+}
+
+#replacing old (below:) -- with new vsn(above:)
+#get_resource_name_by_role(*resc_name, *attr, *value) {
+#     *resc_name = "demoResc"    # -- *resc_name = "NULL" # old D-to-C example
+#     if (*value == "") { # assume *attr is of form "KEY=VALUE"
+#       *out1 = ""
+#       pyParseRoleSpec ( *attr, *out1, *out2 ) # see ~irods/compute/util.py
+#     }
+#     foreach(*row in SELECT DATA_RESC_NAME WHERE META_RESC_ATTR_NAME = '*attr' AND 
+#                     META_RESC_ATTR_VALUE = '*value') {
+#         *resc_name = *row.DATA_RESC_NAME
+#     } # foreach
+# }
 
 rmtExec_slurm ( *host , *arg ) 
 {
--- advanced/hpc_data_to_compute/data_to_compute_X.r
+++ advanced/hpc_data_to_compute/data_to_compute_X.r
@@ -1,21 +1,48 @@
-mainRule {
-  *key = "COMPUTE_RESOURCE_ROLE = IMAGE_PROCESSING"
-  *hostname = ""
+testRule 
+{
+  *hostname = "null"
+  #---
+  *key = "COMPUTE_RESOURCE_ROLE = IMAGE_PROCESSING" ; *val = ""
+  writeLine ("stdout", "key->[*key]  val->[*val] ")
   *resc_name = ""
-#
-  get_host_and_resource_name_by_role(*hostname, *resc_name, *key, *value) 
-  #*out1 = ""; *out2 = "" ; pyParseRoleSpec ( *key, *out1, *out2 )
+  get_host_and_resource_name_by_role(*hostname, *resc_name, *key, *val) 
   writeLine ("stdout", "resc->[*resc_name] host->[*hostname] ")
+  #---
+  *key = "COMPUTE_RESOURCE_ROLE"; *val = "IMAGE_PROCESSING"
+  writeLine ("stdout", "OLD VSN key->[*key]  val->[*val] ")
+  *resc_name = ""
+  get_resource_name_by_role(*resc_name, *key, *val) 
+  writeLine ("stdout", "resc->[*resc_name] ")
 }
 
+###############################################################################
+# OLDER VERSION
+# get_resource_name_by_role(*resc_name, *attr, *value) {
+# 
+#     *resc_name =  "NULL"   # -- *resc_name = "demoResc" # old D-to-C example
+#     #*value = ""
+#     *out1 = *attr
+#     *out2 = *value
+#     if (*value == "") { # assume *attr is of form "KEY=VALUE"
+#       pyParseRoleSpec ( *attr, *out1, *out2 ) # see ~irods/compute/util.py
+#     }
+# 
+#     foreach(*row in SELECT DATA_RESC_NAME WHERE META_RESC_ATTR_NAME = '*out1' AND 
+#                     META_RESC_ATTR_VALUE = '*out2') {
+#         *resc_name = *row.DATA_RESC_NAME
+#     } # foreach
+# }
+# 
+###############################################################################
+# NEWER VERSION
 get_host_and_resource_name_by_role (*host, *resc_name, *attr, *value) {
 
-    *resc_name = "demoResc"    # -- *resc_name = "NULL" # old D-to-C example
-    *value = ""
+    *resc_name = "NULL"    # -- *resc_name = "NULL" # old D-to-C example
+    #*value = ""
 
+    *out1=*attr
+    *out2=*value
     if (*value == "") { # assume *attr is of form "KEY=VALUE"
-      *out1=""
-      *out2=""
       pyParseRoleSpec ( *attr, *out1, *out2 ) # see ~irods/compute/util.py
       writeLine ("stdout", " [ *out1 ] -> [ *out2 ] ")
     }
