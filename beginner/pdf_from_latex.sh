0afc15becf8b9809b36139422d53c531b8ad08b7
d-w-moore <dmoore@renci.org>
2022-06-14 14:27:09 +0000

    more beginner doc updates

--- beginner/pdf_from_latex.sh
+++ beginner/pdf_from_latex.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 CMDDIR=$(dirname "$0")
 cd "$CMDDIR"
+echo -n "Building image..." >&2
 docker build --quiet=true --rm -t pdf_from_latex -f Dockerfile.pdflatex . || {
    echo "Docker must be installed for this script to work." >&2
    exit 1
 }
+echo
+echo "Converting TEX to PDF ..." >&2
 docker run --rm -v `pwd`:/tmp -t pdf_from_latex *.tex
 make clean
