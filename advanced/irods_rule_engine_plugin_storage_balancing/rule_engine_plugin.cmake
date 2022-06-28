02e64822675b4d70a4b9eced3aaa5a6aca67b000
Alan King <alanking@renci.org>
2022-06-28 12:59:24 -0400

    UGM2022: Update CMake for REP

--- advanced/irods_rule_engine_plugin_storage_balancing/rule_engine_plugin.cmake
+++ advanced/irods_rule_engine_plugin_storage_balancing/rule_engine_plugin.cmake
@@ -12,16 +12,15 @@ set(
 add_library(
   ${PLUGIN}
   MODULE
-  ${CMAKE_SOURCE_DIR}/src/libirods_rule_engine_plugin-cpp-storage-balancing.cpp
+  "${CMAKE_CURRENT_SOURCE_DIR}/src/libirods_rule_engine_plugin-cpp-storage-balancing.cpp"
   )
 
 target_include_directories(
   ${PLUGIN}
   PRIVATE
-  ${IRODS_INCLUDE_DIRS}
-  ${IRODS_EXTERNALS_FULLPATH_BOOST}/include
-  ${IRODS_EXTERNALS_FULLPATH_JANSSON}/include
-  ${IRODS_EXTERNALS_FULLPATH_JSON}/include
+  "${IRODS_EXTERNALS_FULLPATH_BOOST}/include"
+  "${IRODS_EXTERNALS_FULLPATH_FMT}/include"
+  "${IRODS_EXTERNALS_FULLPATH_SPDLOG}/include"
   )
 
 target_link_libraries(
@@ -29,16 +28,27 @@ target_link_libraries(
   PRIVATE
   irods_server
   irods_common
-  ${IRODS_EXTERNALS_FULLPATH_BOOST}/lib/libboost_coroutine.so
-  ${IRODS_EXTERNALS_FULLPATH_BOOST}/lib/libboost_system.so
+  "${IRODS_EXTERNALS_FULLPATH_BOOST}/lib/libboost_coroutine.so"
+  "${IRODS_EXTERNALS_FULLPATH_BOOST}/lib/libboost_filesystem.so"
+  "${IRODS_EXTERNALS_FULLPATH_BOOST}/lib/libboost_system.so"
+  "${IRODS_EXTERNALS_FULLPATH_BOOST}/lib/libboost_regex.so"
+  "${IRODS_EXTERNALS_FULLPATH_FMT}/lib/libfmt.so"
+  ${CMAKE_DL_LIBS}
   )
 
-target_compile_definitions(${PLUGIN} PRIVATE ${IRODS_COMPILE_DEFINITIONS_${PLUGIN}} ${IRODS_COMPILE_DEFINITIONS} BOOST_SYSTEM_NO_DEPRECATED)
+target_compile_definitions(
+  ${PLUGIN}
+  PRIVATE
+  ${IRODS_COMPILE_DEFINITIONS_PRIVATE}
+  ENABLE_RE
+  IRODS_ENABLE_SYSLOG
+)
+
 set_property(TARGET ${PLUGIN} PROPERTY CXX_STANDARD ${IRODS_CXX_STANDARD})
 
 install(
   TARGETS
   ${PLUGIN}
   LIBRARY
-  DESTINATION usr/lib/irods/plugins/rule_engines
+  DESTINATION "${IRODS_PLUGINS_DIRECTORY}/rule_engines"
   )
