c0537d3cceb82fdea54da24c32b42199a47d3a78
Alan King <alanking@renci.org>
2023-06-08 17:31:44 -0400

    squash with 'UGM 2023: Advanced Training - Compute to Data updates'

--- advanced/hpc_compute_to_data/compute_to_data.py
+++ advanced/hpc_compute_to_data/compute_to_data.py
@@ -220,7 +220,8 @@ def container_dispatch(rule_args, callback, rei):
                 if vault_paths.get ('output'):
                     docker_opts ['volumes'][docker_host_output_path] = { 'bind': docker_guest_output_path, 'mode': 'rw' }
 
-            # Must run as user 999 (iRODS service account) because iRODS vault uses permission 600
+            # Must run as user jovyan, which maps to the iRODS service account on the host. iRODS vault uses permission
+            # 600 by default, so the iRODS user is the only one that can see the files.
             docker_opts['user'] = 'jovyan'
             # Remove the container on exit
             docker_opts['remove'] = True
